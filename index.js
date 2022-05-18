63d1ed30cef036bddf3a6cc22c7690eee92531d3
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-17 21:57:58 -0400

    commit tree 94f9d851da0925a3bfdc530f5006621d49e453cd

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number){
+
+    return Math.abs(number - 42);
+
+}
+
+
+function distanceFromHqInFeet(someValue){
+
+    return distanceFromHqInBlocks(someValue)*264;
+
+}
+
+
+function distanceTravelledInFeet(passengerStart,passengerDestination) {
+
+    return Math.abs(passengerStart - passengerDestination) * 264;
+
+
+}
+
+function calculatesFarePrice(passengerStart,passengerDestination) {
+//const distanceTravelled = Math.abs(start - destination);
+//const distanceTravelledInFeet = distanceTravelled * 264;
+const ride = distanceTravelledInFeet(passengerStart,passengerDestination)
+if ( ride <  400) {
+    return ride * 0;
+
+}
+
+
+
+
+else if ( ride > 400 && ride <= 2000 )
+
+return (ride - 400) * .02;
+
+
+
+else if ( ride > 2000 && ride < 2500 )
+return 25;
+
+
+
+else  {
+return 'cannot travel that far'
+
+}
+
+}
+
+calculatesFarePrice();
